60eb4a221f766e50b11c75286fe128e9f1b4966d
Luka Jandrijevic <jandrijevic55@gmail.com>
2022-06-10 18:29:20 +0200

    dodao lekser - treba nadopunit malo

--- snail.py
+++ snail.py
@@ -1,39 +1,44 @@
 ''' 
+Domaća zadaća iz kolegija Interpretacija programa.
+Napravili: Ivona Čižić, Barbara Posavec, Luka Jandrijević
 
 '''
-
-
-
 
 from vepar import *
 
 class T(TipoviTokena):
+	PRINT, IF, ELSE, THEN= 'print', 'if', 'else', 'then' 
+	ENDIF, NEWLINE = 'endif', 'newline'
+	OTV, ZATV, TOČKAZAREZ, PLUS, MINUS, PUTA, KROZ, MANJE, VEĆE, JEDNAKO = '();+-*/<>='
+	MANJEJ, VEĆEJ, JEDNAKOJ, RAZLIČITO = '<=','>=', '==', '!='
+	NAVODNICI = '"'
+	KOMENTAR = '\\'
+	class BROJ(Token):
+		#def vrijednost(self): return
+		pass
+	class IME(Token):
+		#def vrijednost(self): return
+		pass
 
+@lexer
+def snail(lex):
+	for znak in lex:
+		if znak.isspace(): lex.zanemari()
+		elif znak == '<': yield lex.token(T.MANJEJ if lex >= '=' else T.MANJE)
+		elif znak == '>': yield lex.token(T.VEĆEJ if lex >= '=' else T.VEĆE)
+		elif znak == '=': yield lex.token(T.JEDNAKOJ if lex >= '=' else T.JEDNAKO)
+		elif znak == '!':
+			if lex >= '=': yield lex.token(T.RAZLIČITO)
+			else: raise lex.greška('iza znaka "!" mora slijediti znak "="')
+		elif znak.isalpha():
+			lex * str.isalnum
+			yield lex.literal_ili(T.IME)
+		elif znak.isdecimal():
+			lex.prirodni_broj(znak)
+			yield lex.token(T.BROJ)
+		else: yield lex.literal(T)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+test = 'print var2 = 3 + var1 * 4 / 73 - b1'
+snail(test)
 
 
